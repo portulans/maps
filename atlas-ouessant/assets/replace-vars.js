6adf1d7127e7d3abcf891cf9675168e9e264b3f2
portulans <solenn.tual@ensg.eu>
2024-12-07 12:41:00 +0100

    Update maps

--- atlas-ouessant/assets/replace-vars.js
+++ atlas-ouessant/assets/replace-vars.js
@@ -2,21 +2,13 @@
   
   var GuestBook = document.getElementById("GuestBook") ;
 GuestBook.innerHTML = GuestBook.innerHTML
-                        .replace(/GOOGLE_ENTRY_ID_Name/g, GOOGLE_ENTRY_ID_Name)
+   .replace(/GOOGLE_ENTRY_ID_Name/g, GOOGLE_ENTRY_ID_Name)
 	
 	.replace(/GOOGLE_ENTRY_ID_Email/g, GOOGLE_ENTRY_ID_Email)
 	
 	.replace(/GOOGLE_ENTRY_ID_TextArea/g, GOOGLE_ENTRY_ID_Guestbook)
 
-        .replace(/GOOGLE_Captcha_Key/g, CaptchaKey)
+  .replace(/GOOGLE_Captcha_Key/g, CaptchaKey)
 
-
-	
-
-	
-	
-	
-	
-	
-                         .replace(/GOOGLE_FORM_URL/g, Google_Form_Link)
-  ;
+  .replace(/GOOGLE_FORM_URL/g, Google_Form_Link)
+;
